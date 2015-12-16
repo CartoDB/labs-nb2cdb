6098f866f5a35114e4b7b8e83903799c6b8869e7
Chris Whong <chris@whong.org>
2015-12-16 13:00:09 -0500

    First Commit

---
+++ config.sample.js
@@ -0,0 +1,11 @@
+var config = {
+  nbApiToken: 'reallylongtokengoeshere',
+  nbSlug: 'nbslug',
+  nbRows: 100,
+  cdbUsername: 'username',
+  cdbApiKey: 'reallylongcartodbapikey',
+  cdbTablename: 'tablename'
+}
+
+
+module.exports = config;
